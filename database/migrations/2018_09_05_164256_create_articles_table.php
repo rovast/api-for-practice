0e5e1385d49a1697b8a4a5fef00d35dee312e789
rovast <rovast@163.com>
2018-09-06 00:52:32 +0800

    完成数据库字段定义

--- database/migrations/2018_09_05_164256_create_articles_table.php
+++ database/migrations/2018_09_05_164256_create_articles_table.php
@@ -15,6 +15,15 @@ class CreateArticlesTable extends Migration
     {
         Schema::create('articles', function (Blueprint $table) {
             $table->increments('id');
+            $table->unsignedInteger('user_id')->comment('作者');
+            $table->unsignedInteger('category_id')->nullable()->comment('文章所属分类');
+            $table->string('title')->nullable()->comment('文章标题');
+            $table->string('cover')->nullable()->comment('文章封面');
+            $table->text('content')->nullable()->comment('文章内容');
+            $table->tinyInteger('status')->default(0)->comment('文章状态 0 草稿  1  正常发布');
+            $table->softDeletes();
+            $table->unsignedInteger('stars')->default(0)->comment('文章点赞数');
+            $table->unsignedInteger('views')->default(0)->comment('文章阅读数');
             $table->timestamps();
         });
     }
