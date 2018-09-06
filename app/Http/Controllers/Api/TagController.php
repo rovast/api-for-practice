9e51e9c61d69d6bb029a52c65f86901b2db331af
rovast <rovast@163.com>
2018-09-07 00:42:45 +0800

    完成tags列表的获取

--- app/Http/Controllers/Api/TagController.php
+++ app/Http/Controllers/Api/TagController.php
@@ -2,9 +2,23 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Http\Resources\TagResource;
+use App\Tag;
 use Illuminate\Http\Request;
 
 class TagController extends ApiController
 {
-    //
+    protected $tag;
+
+    public function __construct(Tag $tag)
+    {
+        $this->tag = $tag;
+    }
+
+    public function index(Request $request)
+    {
+        $data = TagResource::collection($this->tag->all());
+
+        return $this->responseSuccess($data);
+    }
 }
